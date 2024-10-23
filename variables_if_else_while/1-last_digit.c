efe0f6ba5afba58e160eca1efe573807ac0c8d14
darkvalds <9953@holbertonstudents.com>
2024-10-23 19:56:34 +0200

    modif 2-print_alphabet

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	last_digit = n % 10; /* Calcul du dernier chiffre de n */
+	last_digit = n % 10;
 
 	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit > 5)
