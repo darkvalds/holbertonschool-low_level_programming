37651a2f5ee32be3f7e2619ed2a815ed8ee7c49a
darkvalds <9953@holbertonstudents.com>
2024-10-23 11:46:08 +0200

    modif 0-positive_or_negative.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,35 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 
 /**
- * main - Prints the last digit of a random number
- * Description: This program generates a random number
- *              and prints its last digit.
+ * main - Prints the alphabet in lowercase
+ * Description: This program uses the putchar function
+ *              to print the alphabet in lowercase.
  * Return: 0 (Success)
  */
 int main(void)
 {
-    int n;
-    int last_digit;
+    char letter;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    last_digit = n % 10;  // Calcul du dernier chiffre de n
-
-    printf("Last digit of %d is %d ", n, last_digit);
-    if (last_digit > 5)
+    for (letter = 'a'; letter <= 'z'; letter++)
     {
-        printf("and is greater than 5\n");
+        putchar(letter);
     }
-    else if (last_digit == 0)
-    {
-        printf("and is 0\n");
-    }
-    else
-    {
-        printf("and is less than 6 and not 0\n");
-    }
+    putchar('\n');
 
     return (0);
 }
