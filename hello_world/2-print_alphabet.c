4a79e75a5fa146243d9414a0ea998694acb609b8
darkvalds <9953@holbertonstudents.com>
2024-10-23 19:50:35 +0200

    modif 2-print_alphabet

---
+++ hello_world/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the alphabet in lowercase
+ *              followed by a newline. It only uses putchar twice.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
