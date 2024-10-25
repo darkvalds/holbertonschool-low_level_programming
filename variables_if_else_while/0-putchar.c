9c7c8229656221abf4f15b3cda4eedce8a6a00e2
darkvalds <9953@holbertonstudents.com>
2024-10-25 13:06:05 +0200

    create flies

---
+++ variables_if_else_while/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Decription: Print "_putchar" followed by a new line.
+ *
+ * return: Always 0 (Success)
+ */
+itn main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
