28f02132e43205620e519b145196a523ec44a2c0
darkvalds <9953@holbertonstudents.com>
2024-10-27 09:40:02 +0100

    create flies

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *
+ *print_alphabet - prints the alphabet in lowercase, folle
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+	_putchar('\n');
+
+	return 0;
+}
