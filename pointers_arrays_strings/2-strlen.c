3057ac6892aeb702fbda4246498ff578e14a358c
darkvalds <9953@holbertonstudents.com>
2024-10-30 11:08:11 +0100

    creat to files

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,11 +2,17 @@
 
 /**
  *_strlen - Returns the lengh of a string.
- *@str: The string to get the length of.
+ *@s: The string to get the length of.
  *
  * Return: The length of *str.
  */
 
 int _strlen(char *s)
 {
-	char *s
+	int l;
+
+	for (l = 0; s[l] ; l++)
+	{
+	}
+	return (l);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+int _strlen(char *s);
 #endif /* _MAIN_H_*/
