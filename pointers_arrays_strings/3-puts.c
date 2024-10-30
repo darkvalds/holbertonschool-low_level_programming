69ee55ca0f86216a03826f732fcd8d00df3969fe
darkvalds <9953@holbertonstudents.com>
2024-10-30 12:19:57 +0100

    creat to files

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
- *
- *
- *
+ * _puts - Prints a string followed by a new line.
+ * @str: The string to print.
  */
-#define _puts pokemon
-
 void _puts(char *str)
-{ 
-	(void)str;
-  pokemon ("hello");
+{
+	int i;
 
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
