26f26f4d9532c17848a493d1aec944461ea70708
darkvalds <9953@holbertonstudents.com>
2024-11-20 15:20:46 +0100

    comment to fuction pointeur.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,17 +1,22 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/*
+/**
  * File: function_pointers.h
-
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0E-function_pointers directory.
+ * Description: Header file containing prototypes for all functions
+ *              used in the 0x0E-function_pointers directory.
  */
 
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
+
+/* Prototype for a function that prints a name */
 void print_name(char *name, void (*f)(char *));
+
+/* Prototype for a function that executes a function on each element of an array */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/* Prototype for a function that searches for an integer in an array */
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
