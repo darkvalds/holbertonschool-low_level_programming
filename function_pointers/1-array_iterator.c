0380c1c12e8b3070c47646919f3571ca6f767a1b
darkvalds <9953@holbertonstudents.com>
2024-11-20 15:31:00 +0100

    changing the for lop to  wile

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -9,13 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-
 	if (array == NULL || action == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	while (size-- > 0)
 	{
-		action(array[i]);
+		action(*array);
+		array++;
 	}
 }
