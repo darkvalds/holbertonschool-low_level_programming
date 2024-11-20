b4ea6139b76c17d7a55d42baf67ab51da862dd61
darkvalds <9953@holbertonstudents.com>
2024-11-20 14:21:09 +0100

    debug files 1 and 2

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,20 +1,21 @@
 #include "function_pointers.h"
+
 /**
- * array_iterator - executes a function given as a
- * parameter on each element of an array.
- * @array: the array.
- * @size: The size of array.
- * @action: A pointer to the function to bbe executed.
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array to iterate over.
+ * @size: The size of the array.
+ * @action: A pointer to the function to be executed on each element.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
+	size_t i;
 
-if (array == NULL || action == NULL)
-return;
+	if (array == NULL || action == NULL)
+		return;
 
-for (i = 0; i < size; i++)
-{
-action(array[i]);
-}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -6,21 +6,24 @@
  * @size: Number of elements in the array.
  * @cmp: Pointer to the comparison function.
  *
- * Return: Index of the first element for
- * which the cmp function does not return 0.
- *-1 if no element matches or if size <= 0 or if array/cmp is NULL.
+ * Return: Index of the first element for which the cmp function does not return 0.
+ *         -1 if no element matches or if size <= 0 or if array/cmp is NULL.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+	int i;
 
-if (array == NULL || cmp == NULL)
-return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
 
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]) != 0)
-return (i);
-}
-return (-1);
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+
+	return (-1);
 }
