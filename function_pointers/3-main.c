b0642d80d13bfaadb58ad4de42db258f5477cf23
darkvalds <9953@holbertonstudents.com>
2024-11-22 13:03:51 +0100

    bety style corction ùain

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -8,13 +8,11 @@
  * @argv: Array of command-line arguments
  * Return: 0 on success, or specific error codes on failure
  */
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	int (*operator)(int,int);
-    
 
+	int (*operator)(int, int);
 
 if (argc != 4)
 {
@@ -26,18 +24,18 @@ num2 = atoi(argv[3]);
 
 operator = get_op_func(argv[2]);
 	if (operator == NULL)
- {
+{
 	printf("Error\n");
 	return (99);
- }
+}
 
 	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
- {
+{
 	printf("Error\n");
 	return (100);
- }
+}
 	result = operator (num1, num2);
 	printf("%d\n", result);
- 
+
 	return (0);
 }
