1698c669b148484167d731434cbdcc21171ab965
darkvalds <9953@holbertonstudents.com>
2024-11-22 12:52:30 +0100

    checking sytle betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,39 +6,38 @@
  * main - Entry point for the calculator program
  * @argc: Number of command-line arguments
  * @argv: Array of command-line arguments
- * 
  * Return: 0 on success, or specific error codes on failure
  */
 
 int main (int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*operator)(int,int);
+	int num1, num2, result;
+	int (*operator)(int,int);
     
 
 
 if (argc != 4)
 {
-    printf("Error\n");
-    return (98);
+	printf("Error\n");
+	return (98);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[3]);
 
 operator = get_op_func(argv[2]);
- if (operator == NULL)
+	if (operator == NULL)
  {
-    printf("Error\n");
-    return (99);
+	printf("Error\n");
+	return (99);
  }
 
- if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
  {
-    printf("Error\n");
-    return (100);
+	printf("Error\n");
+	return (100);
  }
- result = operator (num1, num2);
- printf("%d\n", result);
+	result = operator (num1, num2);
+	printf("%d\n", result);
  
- return (0);
+	return (0);
 }
