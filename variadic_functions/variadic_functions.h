b7a9cdf87ccfd8b1ea8fa1903f53df38e27ab0cb
darkvalds <9953@holbertonstudents.com>
2024-11-24 08:03:21 +0100

    modified

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,7 @@
 #ifndef VARIADIC_FUCTIONS_H
 #define VARIADIC_FUCTIONS_H
 
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 
 
@@ -9,4 +10,3 @@ int sum_them_all(const unsigned int n, ...);
 
 
 #endif
-
